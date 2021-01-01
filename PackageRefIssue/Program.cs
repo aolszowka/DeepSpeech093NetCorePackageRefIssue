163528bd72957c5f73023d65d659d1229e218721
Ace Olszowka <aolszowka@gmail.com>
2021-01-01 11:02:08 -0700

    Show Cannot Find IDeepSpeech

---
+++ PackageRefIssue/Program.cs
@@ -0,0 +1,31 @@
+﻿using NAudio.Wave;
+
+using System;
+
+namespace PackageRefIssue
+{
+    class Program
+    {
+        private const string DEEPSPEECH_MODEL = "deepspeech-0.9.3-models.pbmm";
+        private const string DEEPSPEECH_SCORER = "deepspeech-0.9.3-models.scorer";
+
+        static void Main(string[] args)
+        {
+            using (IDeepSpeech sttClient = new DeepSpeech(DEEPSPEECH_MODEL))
+            {
+                sttClient.EnableExternalScorer(DEEPSPEECH_SCORER);
+
+                string AudioFilePath = @"audio\2830-3980-0043.wav";
+
+                WaveBuffer waveBuffer = new WaveBuffer(File.ReadAllBytes(AudioFilePath));
+                string speechResult = sttClient.SpeechToText(
+                    waveBuffer.ShortBuffer,
+                    Convert.ToUInt32(waveBuffer.MaxSize / 2));
+
+                Console.WriteLine(speechResult);
+
+                waveBuffer.Clear();
+            }
+        }
+    }
+}
